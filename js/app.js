abc9ca38a531d50727f9ef2520d545010171ec52
Josh Mamroud <josh.mamroud@gmail.com>
2015-12-04 20:05:55 -0500

    Form css updates. Added, but not working, foundation.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-//$(document).foundation();
+$(document).foundation();
 
 (function(jQuery){
-    console.log("File Loaded");
     
     var viewport = {
         width : $(window).width(),
@@ -16,27 +15,55 @@
     	large : 1024,
     	xlarge : 1920
     }
-
-    var jQueryMobileJS = 'http://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js';
-    var jQueryMobileCSS = "http://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js";
-
-    console.log("Wdith: " + viewport.width + " Height: " + viewport.height);
-
-    if( viewport.width <= breakpoint.medium){
-
-    	/*
-    	$('body > div').attr( "data-role", "page");
-    	$('body > div > div').attr( "data-role", "content");
-
-		$.getScript(jQueryMobileJS, function(){
-    		console.log("loaded jQuery Mobile");
-    	});
-
-    	$(".km-toggle-menu").click(function(){
-    		$( "#km-panel-left" ).panel( "open");
-    	});
-		*/
-    }
     
+    var el = document.createElement('div');
+    $("<p>Text</p>").appendTo(".page");
+    $('body > div > div').addClass("inner-wrap");
+    //"<div class='off-canvas-wrap mamroud'>"
+    console.log('Still Here');
     
-})()
+    //if( viewport.width <= breakpoint.medium){
+    //};
+    
+   
+
+})();
+
+$(document).foundation('off-canvas', 'reflow');
+
+
+
+
+
+//var jQueryMobileJS = 'http://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js';
+    //var jQueryMobileCSS = "http://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js";
+
+/*
+$.ajax({
+async:false,
+type:'GET',
+url:source,
+data:null,
+success:console.log('true'),
+dataType:'script',
+error: function(xhr, textStatus, errorThrown) {
+// Look at the `textStatus` and/or `errorThrown` properties.
+}
+});
+*/
+//$('body > div').attr( "data-role", "page");
+//$('body > div > div').attr( "data-role", "content");
+//$('body > div > div').attr( "data-enhance", "false");
+
+//Load jQuery Mobile on mobile devices
+//$.getScript( jQueryMobileJS, function(){ console.log("loaded jQuery Mobile"); });
+/*
+$(".km-toggle-menu").click(function(){
+$( "#km-panel-left" ).panel( "open");
+});
+
+var panels = $('#km-panels');
+console.log(panels);
+$('body > div').append(panels);
+console.log('Appended panels');
+*/
